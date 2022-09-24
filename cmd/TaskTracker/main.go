478bf0e69368944a9cf99ad93ecd230486d64a54
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 16:03:16 +0300

    Used server config file. Added InitConfig method

--- cmd/TaskTracker/main.go
+++ cmd/TaskTracker/main.go
@@ -5,16 +5,29 @@ import (
 	"github.com/gunSlaveUnit/TaskTracker/pkg/repository"
 	"github.com/gunSlaveUnit/TaskTracker/pkg/server"
 	"github.com/gunSlaveUnit/TaskTracker/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("ERROR: failed to initializing config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	serv := service.NewService(repos)
 	hand := handler.NewHandler(serv)
 
 	s := new(server.Server)
-	if err := s.Run("localhost:9876", hand.InitRoutes()); err != nil {
+	address := viper.GetString("host") + ":" + viper.GetString("port")
+	if err := s.Run(address, hand.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running server: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("server")
+
+	return viper.ReadInConfig()
 }
