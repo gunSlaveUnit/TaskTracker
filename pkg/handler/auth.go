00e42942a5123ca9b63eb540ef6cccb0ef38a3e6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 15:36:29 +0300

    Created empty endpoints for auth handler

---
+++ pkg/handler/auth.go
@@ -0,0 +1,15 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) SignUp(cxt *gin.Context) {
+
+}
+
+func (h *Handler) SignIn(cxt *gin.Context) {
+
+}
+
+func (h *Handler) SignOut(cxt *gin.Context) {
+
+}
