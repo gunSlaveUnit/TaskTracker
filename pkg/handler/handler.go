4e40bc22ad63980a04cdec74b14c81d0fa39ff6e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 15:50:33 +0300

    Added Handler constructor

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -9,6 +9,10 @@ type Handler struct {
 	service *service.Service
 }
 
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
+}
+
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
