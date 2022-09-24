e5e738a851f0200cebe3e78c7f8c974282cd0e4e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 15:19:11 +0300

    Added Run method to the server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,3 +7,9 @@ import (
 type Server struct {
 	httpServer *http.Server
 }
+
+func (s *Server) Run(address string) error {
+	s.httpServer = &http.Server{Addr: address}
+
+	return s.httpServer.ListenAndServe()
+}
