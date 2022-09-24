b2ada759a352899692d1b8cbaf39dd9b6ced5196
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 15:56:34 +0300

    Created server object and it's dependencies

--- cmd/TaskTracker/main.go
+++ cmd/TaskTracker/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gunSlaveUnit/TaskTracker/pkg/handler"
+	"github.com/gunSlaveUnit/TaskTracker/pkg/repository"
+	"github.com/gunSlaveUnit/TaskTracker/pkg/server"
+	"github.com/gunSlaveUnit/TaskTracker/pkg/service"
+	"log"
+)
 
+func main() {
+	repos := repository.NewRepository()
+	serv := service.NewService(repos)
+	hand := handler.NewHandler(serv)
+
+	s := new(server.Server)
+	if err := s.Run("localhost:9876", hand.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while running server: %s", err.Error())
+	}
 }
