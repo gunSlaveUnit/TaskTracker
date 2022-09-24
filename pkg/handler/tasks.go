f539a86bbe8e7c9b398db69e5718386c8397a731
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 15:37:59 +0300

    Created empty endpoints for tasks handler

---
+++ pkg/handler/tasks.go
@@ -0,0 +1,23 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) All(cxt *gin.Context) {
+
+}
+
+func (h *Handler) GetBy(cxt *gin.Context) {
+
+}
+
+func (h *Handler) Create(cxt *gin.Context) {
+
+}
+
+func (h *Handler) UpdateBy(cxt *gin.Context) {
+
+}
+
+func (h *Handler) DeleteBy(cxt *gin.Context) {
+
+}
