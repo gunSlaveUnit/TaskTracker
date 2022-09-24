acb80de23f52bcfa6fca318bfd2241a729a46d61
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-24 15:39:44 +0300

    Added a method to create handler routes

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,4 +1,31 @@
 package handler
 
+import "github.com/gin-gonic/gin"
+
 type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	api := router.Group("/api/0.1")
+	{
+		auth := api.Group("/auth")
+		{
+			auth.POST("/sign-up", h.SignUp)
+			auth.POST("/sign-in", h.SignIn)
+			auth.POST("/sign-out", h.SignOut)
+		}
+
+		tasks := api.Group("/tasks")
+		{
+			tasks.GET("/", h.All)
+			tasks.GET("/:id", h.GetBy)
+			tasks.POST("/", h.Create)
+			tasks.PUT("/:id", h.UpdateBy)
+			tasks.DELETE("/:id", h.DeleteBy)
+		}
+	}
+
+	return router
 }
