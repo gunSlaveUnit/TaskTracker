993f9707e9cfdf59093107a728f6aa478e9058ec
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-29 22:20:23 +0300

    Added some simple test code to SignUp method

--- backend/pkg/handler/auth.go
+++ backend/pkg/handler/auth.go
@@ -1,9 +1,20 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gunSlaveUnit/TaskTracker/pkg/entities"
+)
 
 func (h *Handler) SignUp(cxt *gin.Context) {
+	var user entities.User
 
+	if err := cxt.BindJSON(&user); err != nil {
+		return
+	}
+
+	if _, err := h.service.Authorization.CreateUser(user); err != nil {
+		return
+	}
 }
 
 func (h *Handler) SignIn(cxt *gin.Context) {
